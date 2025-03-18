a41e93c03a4efec9f0d7a6009e31a0a5354acb8d
wood.wu <kilokingw@gmail.com>
2025-03-18 14:32:29 +0800

    Update Navbar.tsx

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -94,7 +94,7 @@ const Navbar = () => {
           )}
         </div>
       ) : (
-        <div className="z-20 absolute top-0  bg-black/0 w-full flex items-center justify-between   px-12 pt-14 pb-6 space-x-10 group/navbarC">
+        <div className="z-20 absolute top-0  bg-black/0 w-full flex items-center justify-between   px-12 pt-10 pb-6 space-x-10 group/navbarC">
           {type === "home" && (
             <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-b via-black/30 from-black/50 transition-all opacity-0 group-hover/navbarC:opacity-100 -z-10 pointer-events-none"></div>
           )}
@@ -111,7 +111,7 @@ const Navbar = () => {
               <Image
                 src="https://web.forestdev.work/gaoch/logo.svg"
                 alt="image"
-                width={250}
+                width={330}
                 height={24}
                 priority={false}
               />
